794e24cb62c2dc08d1d801a5b1ac57bc5846a7f2
Erikgithubh <114562425+Erikgithubh@users.noreply.github.com>
2022-11-24 15:19:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,15 +17,15 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'RikZ'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
-global.author = 'WhatsApp Bot'
+global.owner = ['62881025400447']
+global.premium = ['62881025400447']
+global.packname = 'RikZ BOT'
+global.author = 'RikZ'
 global.wlcm = []
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
